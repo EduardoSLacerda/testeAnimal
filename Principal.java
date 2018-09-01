dfb3e7436e8a52671d7b43e26718fbbc39c7ce1b
Eduardo Lacerda <eduardosclacerda@gmail.com>
2018-09-01 00:52:24 -0300

    Aleração

--- Principal.java
+++ Principal.java
@@ -4,17 +4,17 @@ public class Principal {
 
     public static void main(String []args) {
 
-        String teste = JOptionPane.showInputDialog(null, "Qual o animal?");
+        String selecionar = JOptionPane.showInputDialog(null, "Qual o animal?");
 
          Gato gato1 = new Gato();
 
-          if(teste.equalsIgnoreCase("GATO")) {
+          if(selecionar.equalsIgnoreCase("GATO")) {
             gato1.setName(JOptionPane.showInputDialog(null, "Nome do Animal"));
             gato1.setColor(JOptionPane.showInputDialog(null, "Cor do Animal"));
             gato1.setSize(Integer.parseInt(JOptionPane.showInputDialog(null, "Tamanho do Animal")));
             gato1.setCutenessLevel(Integer.parseInt(JOptionPane.showInputDialog(null, "Nível de fofura do Animal")));
         }else{
-            JOptionPane.showMessageDialog(null, "Error");
+            JOptionPane.showMessageDialog(null, "Escreva GATO");
         }
     }
 }
